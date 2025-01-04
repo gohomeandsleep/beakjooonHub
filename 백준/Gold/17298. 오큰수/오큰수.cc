ed00006a9c71a131d385632bc81bb5d9eef5ff10
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2025-01-04 15:37:55 +0900

    [Gold IV] Title: 오큰수, Time: 204 ms, Memory: 9228 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -3,29 +3,13 @@
 
 using namespace std;
 
-void dump(stack<int> s) {
-	stack<int> t;
-	int size = s.size();
-	
-	for (int i = 0; i < size; i++) {
-		int k = s.top();
-		t.push(k);
-		s.pop();
-	}
-
-	for (int i = 0; i < size; i++) {
-		cout << t.top();
-		t.pop();
-	}
-	cout << endl;
-}
-
 int main() {
 	cin.tie(NULL); ios_base::sync_with_stdio(false);
 	int N;
 	cin >> N;
 
 	stack<int> s, result, check;
+
 	for (int i = 0; i < N; i++) {
 		int k;
 		cin >> k;
@@ -36,13 +20,9 @@ int main() {
 	result.push(-1);
 	check.push(s.top());
 	s.pop();
-	//dump(check);
-	//dump(result);
 	
 	for (int i = 0; i < N - 1; i++) {
 		while (!check.empty()) {
-			//system("pause");
-			//cout << check.top() << " : " << s.top() << endl;
 			if (check.top() > s.top()) {
 				//탈출 조건 - check에 값 삽입하고 결과도 삽입
 				result.push(check.top());
@@ -57,8 +37,6 @@ int main() {
 			check.push(s.top());
 		}
 		s.pop();
-		//dump(check);
-		//dump(result);
 	}
 
 	for (int i = 0; i < N; i++) {
