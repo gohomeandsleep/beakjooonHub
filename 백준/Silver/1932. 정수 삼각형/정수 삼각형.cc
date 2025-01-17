43cf7b82dc24048128820bee1c77a59a1b074383
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2025-01-17 13:05:34 +0900

    [Silver I] Title: 정수 삼각형, Time: 40 ms, Memory: 3112 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	vector<vector<int>> arr(N, vector<int>(N, -1));
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j <= i; j++) {
+			int k;
+			cin >> k;
+			if (i > 0) { //덮어 쓸 조건
+				if (j == 0) {
+					arr[i][j] = k + arr[i - 1][j];
+				}
+				else if (j == i) {
+					arr[i][j] = k + arr[i - 1][j - 1];
+				}
+				else {
+					arr[i][j] = max(arr[i - 1][j - 1], arr[i - 1][j]) + k;
+				}
+			}
+			else arr[i][j] = k;
+		}
+	}
+
+	vector<int> result = arr[N - 1];
+	int k = -1;
+	for (int i = 0; i < result.size(); i++) {
+		k = max(k, result[i]);
+	}
+
+	cout << k << endl; 
+
+	return 0;
+	
+}
