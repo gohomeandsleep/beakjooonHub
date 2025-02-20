0e21de9fe5b7c238034bbe8733a63fa498cd40f6
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2025-02-20 09:48:53 +0900

    [Gold IV] Title: 테트로미노, Time: 48 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.cc
+++ 백준/Gold/14500. 테트로미노/테트로미노.cc
@@ -1,71 +1,89 @@
-#include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
+#include <cstdio>
 
 using namespace std;
 
-int block[19][4][2] = { //case, y, x
-    {{0, 0}, {0, 1}, {0, 2}, {0, 3}}, //
-    {{0, 0}, {1, 0}, {2, 0}, {3, 0}}, //
-    {{0, 0}, {0, 1}, {1, 0}, {1, 1}},
-    
-    {{0, 0}, {1, 0}, {2, 0}, {2, 1}},
-    {{0, 1}, {1, 1}, {2, 1}, {2, 0}},
-    {{0, 0}, {0, 1}, {0, 2}, {1, 0}},
-    {{0, 0}, {1, 0}, {1, 1}, {1, 2}},
-    {{0, 0}, {0, 1}, {1, 1}, {2, 1}},
-    {{0, 0}, {0, 1}, {1, 0}, {2, 0}},
-    {{1, 0}, {1, 1}, {1, 2}, {0, 2}},
-    {{0, 0}, {0, 1}, {0, 2}, {1, 2}},
-    
-    {{0, 0}, {1, 0}, {1, 1}, {2, 1}},
-    {{0, 1}, {1, 1}, {1, 0}, {2, 0}},
-    {{1, 0}, {1, 1}, {0, 1}, {0, 2}},
-    {{0, 0}, {0, 1}, {1, 1}, {1, 2}},
-    
-    {{1, 0}, {1, 1}, {1, 2}, {0, 1}},
-    {{0, 0}, {0, 1}, {0, 2}, {1, 1}},
-    {{1, 0}, {0, 1}, {1, 1}, {2, 1}},
-    {{0, 0}, {1, 0}, {2, 0}, {1, 1}},
-};
+int height, width, result, maxValue;
 
-bool inRange(int y, int x, int h, int w){
-    return y >=0 && x >= 0 && y < h && x < w;
+int dy[4] = {1, -1, 0, 0};
+int dx[4] = {0, 0, 1, -1};
+
+bool inRange(int y, int x) {
+	return y >= 0 && x >= 0 && y < height && x < width;
 }
 
-int main(){
-    int height, width;
-    cin >> height >> width;
-    
-    vector<vector<int>> arr(height, vector<int>(width, 0));
-    int k;
-    for (int i = 0; i < height; i++){
-        for (int j = 0; j < width; j++){
-            cin >> k;
-            arr[i][j] = k;
-        }
-    }
-    
-    int result = 0;
-    for (int i = 0; i < height; i++){ //y
-        for (int j = 0; j < width; j++){ //x
-            for (int k = 0; k < 19; k++){ //ternomino
-                bool isValid = true; //valid check
-                for (int l = 0; l < 4; l++){ //single space -> if not valid, pass
-                    if (!inRange(i + block[k][l][0], j + block[k][l][1], height, width)) isValid = false;
-                }
-                if (isValid){
-                    int sum = 0;
-                    for (int l = 0; l < 4; l++){
-                        sum += arr[i + block[k][l][0]][j + block[k][l][1]];
-                    }
-                    result = max(result, sum);
-                }
-            }
-        }
-    }
-    
-    cout << result << endl;
-    
-    return 0;
+void dfs(const vector<vector<int>> &arr, int y, int x, vector<vector<bool>> &visited, int depth, int psum) {
+	if (psum + (3 - depth) * maxValue <= result)
+		return;
+	if (depth == 3) {
+		result = max(result, psum);
+		return;
+	}
+	for (int i = 0; i < 4; i++) {
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (inRange(ny, nx) && !visited[ny][nx]) {
+			visited[ny][nx] = 1;
+			dfs(arr, ny, nx, visited, depth + 1, psum + arr[ny][nx]);
+			visited[ny][nx] = 0;
+		}
+	}
+}
+
+void bob(const vector<vector<int>> &arr, int y, int x) { //
+	vector<int> avail;
+	int psum = arr[y][x];
+	int n = 0;
+	for (int i = 0; i < 4; i++) {
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (inRange(ny, nx)) {
+			n++;
+			psum += arr[ny][nx];
+			avail.push_back(arr[ny][nx]);
+		}
+	}
+
+	if (n == 4) {
+		result = max(result, psum - *min_element(avail.begin(), avail.end()));
+	}
+	else if (n == 3) {
+		result = max(result, psum);
+	}
+}
+
+int main() {
+	scanf("%d %d", &height, &width);
+
+	vector<vector<int>> arr(height, vector<int> (width, 0));
+	int k;
+
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			scanf("%d", &k);
+			arr[i][j] = k;
+		}
+	}
+
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			maxValue = max(maxValue, arr[i][j]);
+		}
+	}
+
+	vector<vector<bool>> visited(height, vector<bool> (width, false));
+	result = 0;
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			visited[i][j] = 1;
+			dfs(arr, i, j, visited, 0, arr[i][j]);
+			bob(arr, i, j);
+			visited[i][j] = 0;
+		}
+	}
+
+	printf("%d", result);
+
+	return 0;
 }
